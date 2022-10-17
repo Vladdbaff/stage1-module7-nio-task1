f208c62aefbf38372312c42da2f141eeb11eb878
vladislav <vladdbaf@mail.ru>
2022-10-17 15:43:08 +0300

    task is completed

--- src/main/java/com/epam/mjc/nio/Main.java
+++ src/main/java/com/epam/mjc/nio/Main.java
@@ -2,12 +2,16 @@ package com.epam.mjc.nio;
 
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args){
         FileReader fileReader = new FileReader();
         File file = fileReader.getFileFromResources("Profile.txt");
-        System.out.println(fileReader.getDataFromFile(file));
+        logger.log(Level.INFO, fileReader.getDataFromFile(file).toString());
     }
 }
